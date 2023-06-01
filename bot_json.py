c0a0de59b3d8f03459587fbf52831d42f7cc7d94
PY079 <n.cowalyow2017@yandex.ru>
2023-06-01 15:12:01 +0300

    red

--- bot_json.py
+++ bot_json.py
@@ -2,7 +2,7 @@ import telebot
 from data import token_tg_b, id_chat
 
 bot = telebot.TeleBot(token_tg_b)
-@bot.message_handler(content_types=['photo','video','audio','document','audio'])
+@bot.message_handler(content_types=['photo','video','audio','document'])
 def handle_text(message):
     if message.photo:
         # Обработка фото
@@ -10,7 +10,7 @@ def handle_text(message):
         file_id = photo.file_id  # Идентификатор файла фото
         file_unique_id = photo.file_unique_id  # Уникальный идентификатор файла фото
         file_size = photo.file_size  # Размер файла фото
-
+        bot.send_photo(id_chat ,caption=f'тест photo\n{message.caption}', photo=file_id)
         # Ваш код обработки фото
 
     elif message.video:
@@ -19,6 +19,7 @@ def handle_text(message):
         file_id = video.file_id
         file_unique_id = video.file_unique_id
         file_size = video.file_size
+        bot.send_video(id_chat ,caption=f'тест video\n{message.caption}', video=file_id)
 
         # Ваш код обработки видео
 
@@ -28,6 +29,7 @@ def handle_text(message):
         file_id = audio.file_id
         file_unique_id = audio.file_unique_id
         file_size = audio.file_size
+        bot.send_audio(id_chat ,caption=f'тест audio\n{message.caption}', audio=file_id)
 
         # Ваш код обработки аудио
 
@@ -37,10 +39,10 @@ def handle_text(message):
         file_id = document.file_id
         file_unique_id = document.file_unique_id
         file_size = document.file_size
-
+        bot.send_document(id_chat ,caption=f'тест document\n{message.caption}', document=file_id)
         # Ваш код обработки документа
     print(file_id)
-    bot.send_audio(id_chat ,caption='тест', audio=file_id)
+    
 
 
 
